7b7a6c49030b64b5346d042518c603cd4401c248
Benjamin Giacobbi <benjamingiacobbi@gmail.com>
2020-09-18 22:31:19 -0500

    Standardized Double Quote Format
    All strings now use double quote, previously inconsistent

--- src/scenefile.py
+++ src/scenefile.py
@@ -6,10 +6,10 @@ class SceneFile(object):
     """Abstract representation of a scene file"""
     def __init__(self, path):
         self.folder_path = Path()
-        self.descriptor = 'main'
-        self.task = 'none'
+        self.descriptor = "main"
+        self.task = "none"
         self.version = 1
-        self.version = '.ma'
+        self.version = ".ma"
         self._init_from_path(path)
 
     @property
@@ -27,16 +27,16 @@ class SceneFile(object):
     def _init_from_path(self, path):
         path = Path(path)
         self.folder_path = path.parent
-        property_list = re.findall(r'([^\W_v]+|\.[A-Za-z0-9]+)', path.name)
+        property_list = re.findall(r"([^\W_v]+|\.[A-Za-z0-9]+)", path.name)
         self.descriptor, self.task, ver, self.ext = property_list
         self.ver = int(ver.lstrip("0"))
 
 
 scene_file = SceneFile("D:/assets/tank_model_v001.ma")
-print(scene_file.path)
 print(scene_file.folder_path)
-print(scene_file.filename)
 print(scene_file.descriptor)
 print(scene_file.task)
 print(scene_file.ver)
 print(scene_file.ext)
+print(scene_file.path)
+print(scene_file.filename)
