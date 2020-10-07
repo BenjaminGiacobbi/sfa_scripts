5584dd78e5e6abcf5f06c6d192383beedbf20445
Benjamin Giacobbi <benjamingiacobbi@gmail.com>
2020-10-07 17:06:07 -0500

    Integrated scenefile with smartsave + revamped filename display

--- src/smartsave.py
+++ src/smartsave.py
@@ -19,14 +19,14 @@ def maya_main_window():
 
 def create_button_stylesheet():
     stylesheet = """
-        QWidget {
+        QPushButton {
             font-weight: bold;
             color: #000000;
             border: 2px solid #FFCA1C;
             background-color: #EABF3C;
             border-radius: 1px;
         }
-        QWidget:hover {
+        QPushButton:hover {
             background-color: #FFD147;
         }"""
     return stylesheet
@@ -41,55 +41,60 @@ class SmartSaveUI(QtWidgets.QDialog):
         self.setMaximumHeight(200)
         self.setWindowFlags(self.windowFlags() ^
                             QtCore.Qt.WindowContextHelpButtonHint)
+        self.scene_file = SceneFile()
         self.create_ui()
+        self.create_connections()
 
     def create_ui(self):
         # need to find out a solution to prevent this warning
         self.title_lbl = QtWidgets.QLabel("Smart Save")
         self.title_lbl.setStyleSheet("font: 20px")
+
         self.folder_lay = self._create_folder_ui()
         self.filename_lay = self._create_filename_ui()
         self.display_lay = self._create_filename_display()
         self.button_lay = self._create_buttons_ui()
         self.main_lay = QtWidgets.QVBoxLayout()
+
         self.main_lay.addWidget(self.title_lbl)
         self.main_lay.addLayout(self.folder_lay)
         self.main_lay.addLayout(self.filename_lay)
         self.main_lay.addLayout(self.display_lay)
         self.main_lay.addLayout(self.button_lay)
-
-        # need to find a way to cancel timer upon program exit
-        self._update_timer = QtCore.QTimer()
-        self._update_timer.timeout.connect(self._update_filename_display)
-        self._update_timer.start(50)
         self.setLayout(self.main_lay)
 
     def _create_folder_ui(self):
         default_folder = Path(cmds.workspace(query=True, rootDirectory=True))
         default_folder = default_folder / "scenes"
+
         self.folder_le = QtWidgets.QLineEdit(default_folder)
         self.folder_le.setMinimumHeight(30)
         self.folder_browse_btn = QtWidgets.QPushButton("Browse")
         self.folder_browse_btn.setMinimumHeight(30)
+
         layout = QtWidgets.QHBoxLayout()
         layout.addWidget(self.folder_le)
         layout.addWidget(self.folder_browse_btn)
         return layout
 
     def _create_filename_ui(self):
-        layout = self._create_filename_headers()
-        self.descriptor_le = QtWidgets.QLineEdit("main")
+        self.descriptor_le = QtWidgets.QLineEdit(self.scene_file.descriptor)
+        self.descriptor_le.setMaxLength(30)
         self.descriptor_le.setMinimumWidth(100)
         self.descriptor_le.setMinimumHeight(30)
-        self.task_le = QtWidgets.QLineEdit("model")
+
+        self.task_le = QtWidgets.QLineEdit(self.scene_file.task)
+        self.task_le.setMaxLength(10)
         self.task_le.setFixedWidth(100)
         self.task_le.setMinimumHeight(30)
+
         self.ver_sbx = QtWidgets.QSpinBox()
-        self.ver_sbx.setValue(1)
+        self.ver_sbx.setValue(self.scene_file.ver)
         self.ver_sbx.setButtonSymbols(QtWidgets.QAbstractSpinBox.PlusMinus)
-        self.ver_sbx.setDisplayIntegerBase(5)
         self.ver_sbx.setFixedWidth(50)
         self.ver_sbx.setMinimumHeight(30)
+
+        layout = self._create_filename_headers()
         layout.addWidget(self.task_le, 1, 2)
         layout.addWidget(self.ver_sbx, 1, 4)
         layout.addWidget(self.descriptor_le, 1, 0)
@@ -102,6 +107,7 @@ class SmartSaveUI(QtWidgets.QDialog):
         self.task_lbl.setStyleSheet("font-weight: bold")
         self.ver_lbl = QtWidgets.QLabel("Version")
         self.ver_lbl.setStyleSheet("font-weight: bold")
+
         layout = QtWidgets.QGridLayout()
         layout.addWidget(self.descriptor_lbl, 0, 0)
         layout.addWidget(self.task_lbl, 0, 2)
@@ -111,6 +117,7 @@ class SmartSaveUI(QtWidgets.QDialog):
     def _create_filename_display(self):
         self.filename_lbl = QtWidgets.QLabel("Saves file as: ")
         self.filename_comp = QtWidgets.QLabel("")
+
         layout = QtWidgets.QHBoxLayout()
         layout.addWidget(self.filename_lbl)
         layout.addWidget(self.filename_comp)
@@ -119,38 +126,57 @@ class SmartSaveUI(QtWidgets.QDialog):
     def _create_buttons_ui(self):
         self.save_btn = QtWidgets.QPushButton("Save")
         self.save_btn.setMinimumHeight(40)
+
         self.save_increment_btn = QtWidgets.QPushButton("Save Increment")
-        stylesheet = create_button_stylesheet()
-        self.save_increment_btn.setStyleSheet(stylesheet)
+        self.save_increment_btn.setStyleSheet(create_button_stylesheet())
         self.save_increment_btn.setMinimumHeight(40)
+
         layout = QtWidgets.QHBoxLayout()
         layout.addWidget(self.save_btn)
         layout.addWidget(self.save_increment_btn)
         return layout
 
-    def _update_filename_display(self):
-        spinbox_val = self.ver_sbx.value()
-        name_str = "{descriptor}_{task}_v{ver:03d}.ma"
-        name_str = name_str.format(descriptor=self.descriptor_le.text(),
-                                   task=self.task_le.text(),
-                                   ver=spinbox_val)
+    def create_connections(self):
+        self.descriptor_le.textEdited.connect(self._update_descriptor_display)
+        self.task_le.textEdited.connect(self._update_task_display)
+
+    def _update_descriptor_display(self):
+        if self.descriptor_le.text().__len__() < 1:
+            self._update_filename_display(desc_val=self.scene_file.descriptor)
+        else:
+            self._update_filename_display(desc_val=self.descriptor_le.text())
+
+    def _update_task_display(self):
+        if self.task_le.text().__len__() < 1:
+            self._update_filename_display(task_val=self.scene_file.task)
+        else:
+            self._update_filename_display(task_val=self.task_le.text())
+
+    def _update_filename_display(self,
+                                 desc_val='main',
+                                 task_val='model',
+                                 ver_val=1):
+        name_str = "{desc}_{task}_v{ver:03d}.ma"
+        name_str = name_str.format(desc=desc_val,
+                                   task=task_val,
+                                   ver=ver_val)
         self.filename_comp.setText(name_str)
 
 
 class SceneFile(object):
     """Abstract representation of a scene file"""
     def __init__(self, path=None):
-        self.folder_path = Path()
+        self.folder_path = Path(cmds.workspace(query=True,
+                                               rootDirectory=True)) / "scenes"
         self.descriptor = "main"
-        self.task = "none"
+        self.task = "model"
         self.ver = 1
         self.ext = ".ma"
         scene = pmc.system.sceneName()
         if not path and scene:
             path = scene
         if not path and not scene:
-            log.warning("Unable to initialize SceneFile object"
-                        "from a new scene. Please specify a path.")
+            log.info("Initialize with default properties.")
             return
         self._init_from_path(path)
 
