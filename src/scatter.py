583aab44b752e479d6dd319e42a6ca98285757c2
Benjamin Giacobbi <benjamingiacobbi@gmail.com>
2020-10-16 22:14:47 -0500

    Added method to average normals for a given vertex

--- src/scatter.py
+++ src/scatter.py
@@ -47,10 +47,12 @@ class ScatterTool(object):
 
     def set_scatter_obj(self):
         self.scatter_obj = self.get_object_from_selection()
+        print("Object: " + self.scatter_obj)
 
     def set_scatter_target(self):
         """Retrieves vertices from selection and fills scatter_target"""
         self.scatter_target = self.get_object_from_selection()
+        print("Target: " + self.scatter_target)
         if self.scatter_target:
             vertices = cmds.ls(
                 "{}.vtx[:]".format(self.scatter_target), fl=True)
@@ -68,9 +70,49 @@ class ScatterTool(object):
             return
         scattered = []
         for vert in self.scatter_vertices:
+            # TODO find out why instance returns as a tuple
             instance = cmds.instance(self.scatter_obj)
             pos = cmds.pointPosition(vert, world=True)
-            cmds.move(float(pos[0]), float(pos[1]), float(pos[2]), instance, a=True)
+            cmds.move(pos[0], pos[1], pos[2], instance, a=True)
             scattered.append(instance[0])
+            cmds.select(vert, r=True)
+            vtx_normals = cmds.polyNormalPerVertex(query=True, xyz=True)
+            avg_normal = self.average_normals(vtx_normals)
+            rotation_xyz = self.set_rotation_from_normal(avg_normal)
+            cmds.select(instance[0], r=True)
+            cmds.rotate(rotation_xyz[0], rotation_xyz[1], rotation_xyz[2],
+                        a=True)
+
         scattered_group = cmds.group(scattered, name="scattered_grp")
         return scattered_group
+
+    def average_normals(self, normal_list):
+        """Calculates the average normal of a set of normal values given in
+        a successive list
+
+        Return:
+            List: A list of normal values x, y, and z"""
+        normal_xyz = []
+        for outer_count in range(3):
+            dir_sum = 0
+            dir_count = len(normal_list) // 3
+            for inner_count in range(dir_count):
+                coord_iterator = outer_count + (3 * inner_count)
+                dir_sum = dir_sum + normal_list[coord_iterator]
+            normal_xyz.append(dir_sum / dir_count)
+        return normal_xyz
+
+    def set_rotation_from_normal(self, normal_xyz):
+        """Converts averaged normal vectors to rotation eulers
+
+        Return:
+            List: A list of rotation values x, y, and z"""
+        # rotation_list = []
+        # for counter in range(3):
+        #   if normal_xyz[counter] < 0:
+        #       rotation_modifier = -180
+        #    else:
+        #        rotation_modifier = 180
+        #    normal_xyz[counter] = normal_xyz[counter] * rotation_modifier
+        #    rotation_list.append(normal_xyz[counter])
+        return [0, 0, 0]
